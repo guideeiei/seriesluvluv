aac6525a922d9e61ef7e564e22a715a8058402f1
guideeiei <precharat601@gmail.com>
2025-12-28 14:10:19 +0700

    Fix password check function and update redirects

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function checkpassword() {
+function checkPassword() {
   const input = document.getElementById("password").value;
   const correctPassword = "04122541";
 
   if (input === correctPassword) {
-    window.location.href = "home.html";
+    window.location.href = "success.html";
   } else {
-    document.getElementById("error").innerText = "Wrong date";
+    document.getElementById("error").innerText = "Wrong password 😢";
   }
 }
